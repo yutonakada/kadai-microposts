1a021d5685ad5cf6c1f75a38e8a981157590032d
yuto.nakada <dondoruma.net@gmail.com>
2019-07-24 15:34:57 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Sign up</h1>
+    </div>
+    
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+            
+            {!! Form::open(['route' => 'signup.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Name') !!}
+                    {!! Form::text('name', old('name'), ['class' => 'form-cotrol']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-cotrol']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('password', 'password') !!}
+                    {!! Form::password('password', ['class' => 'form-cotrol']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('password_confirmation', 'password_confirmation') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-cotrol']) !!}
+                </div>
+                    
+                {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
