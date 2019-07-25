2c43146389f1803a21b7f8561dc5f8deeea1ced6
yuto.nakada <dondoruma.net@gmail.com>
2019-07-25 01:47:02 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('id', 'desc')->paginate(10);
+        
+        return view('users,index', [
+            'users' => $users,
+        ]);
+    }
+    
+    //ユーザの特定
+    public function show($id)
+    {
+        $user = User::find($id);
+        
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,12 +11,12 @@
             {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email', 'Email') !!}
-                    {!! Form::label('email', old('email'), ['class' => 'form-control']) !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
                     
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::label('password', ['class' => 'form-control']) !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                     
                 {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
