256eb12accdaa9b671615872a113a89eefd64e5a
yuto.nakada <dondoruma.net@gmail.com>
2019-07-25 03:09:41 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,23 @@
 
 @section('content')
     @if (Auth::check())
-        {{ Auth::user()->name }}
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="class-body">
+                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}", alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Controllerを経由してwelcomeを表示
+Route::get('/', 'MicropostsController@index');
 
 //ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -27,4 +26,5 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 //ユーザ機能
 Route::group(['middleware' => ['auth']], function () {
    Route::resource('users', 'UsersCotroller', ['only' => ['index', 'show']]); 
+   Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
