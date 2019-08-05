9a71f73968308f2a4f1bb8f02dac4d0f8e7de12c
yuto.nakada <dondoruma.net@gmail.com>
2019-08-05 08:55:04 +0000

    register fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,22 +11,22 @@
             {!! Form::open(['route' => 'signup.post']) !!}
                 <div class="form-group">
                     {!! Form::label('name', 'Name') !!}
-                    {!! Form::text('name', old('name'), ['class' => 'form-cotrol']) !!}
+                    {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
                     
                 <div class="form-group">
                     {!! Form::label('email', 'Email') !!}
-                    {!! Form::email('email', old('email'), ['class' => 'form-cotrol']) !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
                     
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::password('password', ['class' => 'form-cotrol']) !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                     
                 <div class="form-group">
                     {!! Form::label('password_confirmation', 'Password_confirmation') !!}
-                    {!! Form::password('password_confirmation', ['class' => 'form-cotrol']) !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
                     
                 {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
